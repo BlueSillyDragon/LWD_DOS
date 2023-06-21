10f89e8c03439a9eb37b5d3b9fa3117764783d1f
BlueSillyDragon <137140267+BlueSillyDragon@users.noreply.github.com>
2023-06-21 12:08:24 -0600

    Update LWDKRNL.asm

--- source/LWDKRNL.asm
+++ source/LWDKRNL.asm
@@ -6,18 +6,17 @@
 [org 0x0]
 [bits 16]
 
-%include "syscalls/print.asm"
-
 kernel_start:
 
     mov ah, 0x00
     mov al, 0x03
-    int 0x13
+    int 0x10
 
     mov si, krnl_msg_loaded
     call os_print_string
 
     jmp $
 
+%include "syscalls/print.asm"
 
 krnl_msg_loaded db "LWD_DOS was successfully loaded!"
