9f5103baf9f7164673ca742d7f55007dd1d99ce5
BlueSillyDragon <137140267+BlueSillyDragon@users.noreply.github.com>
2023-12-13 22:57:27 -0700

    Update SETUP.asm
    SETUP now executes the old setup_command's code.

--- source/programs/SETUP.asm
+++ source/programs/SETUP.asm
@@ -20,7 +20,45 @@ int 0x10
 mov si, msg_setup_environment
 call os_print_string
 
+call os_print_new_line
+call os_print_new_line
+
+mov si, msg_input_month
+call os_print_string
+
+call os_input_string
+
+call os_print_new_line
+
+mov si, msg_input_day
+call os_print_string
+
+call os_input_string
+
+call os_print_new_line
+
+mov si, msg_input_year
+call os_print_string
+
+call os_input_string
+
+call os_print_new_line
+
+mov si, msg_setup_done
+call os_print_string
+call os_keystroke
+
+mov ax, 0x00
+int 0x19
+
+hlt
+
 %include "syscalls/print.asm"
 %include "syscalls/screen.asm"
+%include "syscalls/keyboard.asm"
 
 msg_setup_environment db "LWD-DOS SETUP ENVIRONMENT", 0x0d, 0x0a, 0
+msg_input_month db "What month is it?", 0x0d, 0x0a, 0
+msg_input_day db "What day is it?", 0x0d, 0x0a, 0
+msg_input_year db "What year is it?", 0x0d, 0x0a, 0
+msg_setup_done db "Setup completed successfully, press any key to restart the system", 0x0d, 0x0a, 0
