7498d2465bd98172b178228b8956863d935cbd2b
BlueSillyDragon <BlueSillyDragon@outlook.com>
2024-09-01 23:29:01 -0600

    Added error.asm and changed how 'help' displays commands

--- source/LWDKRNL.asm
+++ source/LWDKRNL.asm
@@ -83,6 +83,10 @@ command_line_interface:
     call os_compare_strings
     jc near execute_setup_program
 
+    mov di, bsod_str
+    call os_compare_strings
+    jc near bsod_command
+
     ; See if the user is trying to execute the kernel file
     mov di, KERNEL_FILENAME
     call os_compare_strings
@@ -107,8 +111,10 @@ command_line_interface:
     hlt
     jmp .halt
 
-
+;========================
 ; Commands
+;========================
+
 about_command:
     call os_print_new_line
     mov si, krnl_msg_about
@@ -119,7 +125,23 @@ about_command:
 
 help_command:
     call os_print_new_line
-    mov si, krnl_msg_help
+    mov si, krnl_msg_help1
+    call os_print_string
+    mov si, krnl_msg_help2
+    call os_print_string
+    mov si, krnl_msg_help3
+    call os_print_string
+    mov si, krnl_msg_help4
+    call os_print_string
+    mov si, krnl_msg_help5
+    call os_print_string
+    mov si, krnl_msg_help6
+    call os_print_string
+    mov si, krnl_msg_help7
+    call os_print_string
+    mov si, krnl_msg_help8
+    call os_print_string
+    mov si, krnl_msg_help9
     call os_print_string
     call os_print_new_line
 
@@ -210,6 +232,19 @@ list_working_directory_command:
 
     jmp command_line_interface
 
+bsod_command:
+    
+    pusha
+
+    mov bx, 0x0a
+    mov [error_code], bx
+
+    call bsod
+
+    popa
+
+    jmp command_line_interface
+
 kernel_execution_attempt:
     mov si, krnl_msg_kernel_exec_attempt
     call os_print_new_line
@@ -234,6 +269,7 @@ os_floppy_error:
 %include "syscalls/strings.asm"
 %include "syscalls/screen.asm"
 %include "syscalls/disk.asm"
+%include "syscalls/error.asm"
 
 prompt db "A:\>", 0               ; This is just what appears before your cursor
 
@@ -247,15 +283,17 @@ echo_str db "echo", 0
 dir_str db "dir", 0
 setup_str db "setup", 0
 time_str db "time", 0
+bsod_str db "bsod", 0
 
 input times 32 db 0             ; Since we're going to be putting our input into here, we need to set aside some bytes for the input
 filename times 11 db 0
+error_code db 0
 
 ; Kernel's messages
 
 krnl_msg_loaded db "LWD-DOS was successfully loaded!", 0x0d, 0x0a, 0
-krnl_msg_ver db "LWD-DOS Version 1.0 Copyright(c) BlueSillyDragon 2023", 0x0d, 0x0a, 0
-krnl_msg_about db "Version: LWD-DOS 1.0, Running in 16-bit real mode, Made in 2023", 0x0d, 0x0a, 0
+krnl_msg_ver db "LWD-DOS Version 1.1 Copyright(c) BlueSillyDragon 2023", 0x0d, 0x0a, 0
+krnl_msg_about db "Version: LWD-DOS 1.1.3.9809012024, Running in 16-bit real mode, Made in 2023", 0x0d, 0x0a, 0
 krnl_msg_type_setup db "Type 'setup' to set the date", 0x0d, 0x0a, 
 
 krnl_msg_type_help db "Type 'help' for a list of commands.", 0x0d, 0x0a, 0
@@ -278,7 +316,15 @@ krnl_msg_invalid_option db "INVALID OPTION! PLEASE TRY AGAIN! (Make sure it's in
 krnl_msg_kernel_exec_attempt db "You cannot run the KERNEL file!", 0x0d, 0x0a, 0
 krnl_msg_file_not_found db "No such file or program!", 0x0d, 0x0a, 0
 
-krnl_msg_help db "COMMANDS: about, help, ver, setup, cls, echo, dir, restart", 0x0d, 0x0a, 0
+krnl_msg_help1 db "COMMANDS:", 0x0d, 0x0a, 0
+krnl_msg_help2 db "about - Tells you about the OS", 0x0d, 0x0a, 0
+krnl_msg_help3 db "help - Prints a list of all commands", 0x0d, 0x0a, 0
+krnl_msg_help4 db "ver - Shows you the shortend version number, for full version use 'about'", 0x0d, 0x0a, 0
+krnl_msg_help5 db "setup - Launches SETUP.BIN", 0x0d, 0x0a, 0
+krnl_msg_help6 db "cls - Clears the screen", 0x0d, 0x0a, 0
+krnl_msg_help7 db "echo - Prints given string to screen", 0x0d, 0x0a, 0
+krnl_msg_help8 db "dir - Shows current directory", 0x0d, 0x0a, 0
+krnl_msg_help9 db "restart - Restarts the system", 0x0d, 0x0a, 0
 
 krnl_msg_invalid db "No such command!", 0x0d, 0x0a, 0
 krnl_msg_floppy_error db "FATAL ERROR TRYING TO READ FROM DISK! PRESS ANY BUTTON TO RESTART!", 0x0d, 0x0a, 0
---
+++ syscalls/error.asm
@@ -0,0 +1,89 @@
+;==============================================================
+; The LWD Disk Operating System Error System Calls.
+; Copyright (c) 2023 BlueSillyDragon
+;==============================================================
+
+;==============================================================
+; Blue Screen of Death
+;==============================================================
+
+; Takes BL as an error code (eg. BL = 0x02 is a disk error)
+
+bsod:
+    mov ah, 0x06
+    mov al, 0x00
+    mov bh, 0x1f
+    mov cx, 0
+    mov dh, 24
+    mov dl, 79
+    int 0x10
+
+    mov dx, 20
+    mov dh, 10
+    call os_move_cursor
+
+    mov si, system_error_message
+    call os_print_string
+    call os_print_new_line
+
+    mov dx, 2
+    mov dh, 11
+    call os_move_cursor
+
+    mov si, system_error_message2
+    call os_print_string
+    call os_print_new_line
+
+    mov dx, 16
+    mov dh, 12
+    call os_move_cursor
+
+    mov si, ctrl_alt_del_message
+    call os_print_string
+    call os_print_new_line
+
+check_for_error_code:
+
+    push bx
+
+    mov bx, [error_code]
+
+    cmp bx, 0x0a
+    je forcefully_invoked
+
+continue:
+
+    mov dx, 26
+    mov dh, 16
+    call os_move_cursor
+
+    mov si, restart_message
+    call os_print_string
+    call os_print_new_line
+
+    call os_keystroke
+
+    mov ax, 0x00
+    int 0x19
+
+forcefully_invoked:
+    mov dx, 20
+    mov dh, 14
+    call os_move_cursor
+    mov si, forcefully_invoked_message
+    call os_print_string
+    call os_print_new_line
+    jmp continue
+
+disk_issue:
+    mov si, disk_issue_message
+    call os_print_string
+    call os_print_new_line
+    jmp continue
+
+system_error_message db "LWD-DOS has ran into a fatal error!", 0
+system_error_message2 db "Your Operating System has been stopped to prevent damage to your computer.", 0
+ctrl_alt_del_message db "*Press Ctrl + Alt + Del to restart your system", 0
+forcefully_invoked_message db "Error code 0x0A - Forcefully Invoked!", 0
+disk_issue_message db "Error code 0x02 - Disk Read Error!", 0
+restart_message db "Press any key to continue", 0
